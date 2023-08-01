087bd34cb18e3271996d3b31e23eeeef5f25e482
TheEpicGamer274 <102255081+TheEpicGamer274@users.noreply.github.com>
2023-07-31 23:05:10 -0400

    Saving

--- src/main/java/gregtech/api/util/GT_Recipe.java
+++ src/main/java/gregtech/api/util/GT_Recipe.java
@@ -3102,7 +3102,7 @@ public class GT_Recipe implements Comparable<GT_Recipe> {
             1,
             0,
             1,
-            E,
+            "Casing Tier : ",
             1,
             E,
             true,
